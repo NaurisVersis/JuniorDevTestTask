79dbe5cf8dbdca88fbda0b1d4428036083bf51f1
NaurisVersis <nauris.versis@gmail.com>
2022-11-22 18:14:30 +0200

    styling

--- public/index.php
+++ public/index.php
@@ -2,7 +2,6 @@
 include __DIR__ . '/../app/bootstrap.php';
 include __DIR__ . '/../app/routing.php';
 
-//echo "Hello World";
 
 $request = Laminas\Diactoros\ServerRequestFactory::fromGlobals(
     $_SERVER,
@@ -11,13 +10,6 @@ $request = Laminas\Diactoros\ServerRequestFactory::fromGlobals(
     $_COOKIE,
     $_FILES,
 );
-
-//$container = new League\Container\Container();
-
-//
-//$strategy = (new League\Route\Strategy\ApplicationStrategy)->setContainer($container);
-//$router = (new League\Route\Router())->setStrategy($strategy);
-
 
 $response = $router->dispatch($request);
 
